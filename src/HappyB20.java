bd8022a4172dcfa2422b4fe416cb1641fc60501c
dianazhunussova <69069471+dianazhunussova@users.noreply.github.com>
2020-08-01 15:35:52 -0500

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,6 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("Happy B20");
         System.out.println("B20 is the coolest batch");
+        // I'm checking git fetch and git merge
     }
 }
